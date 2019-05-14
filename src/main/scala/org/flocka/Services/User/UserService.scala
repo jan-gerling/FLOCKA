300acbe9d61d07c22a042848703c54eff3b42c43
Jan Gerling <jan.gerling@posteo.de>
2019-05-14 23:37:05 +0200

    finalized draft user service

--- src/main/scala/org/flocka/Services/User/UserService.scala
+++ src/main/scala/org/flocka/Services/User/UserService.scala
@@ -1,15 +1,16 @@
 package org.flocka.Services.User
 
-import akka.actor.ActorSystem
-import akka.pattern.ask
+import akka.actor.{ActorRef, ActorSelection, ActorSystem, Identify}
 import akka.http.scaladsl.Http
 import akka.http.scaladsl.server.Directives._
 import akka.http.scaladsl.server.Route
 import akka.stream.ActorMaterializer
-import UserMsg._
+import UserCommunication._
+import akka.pattern.ask
+import akka.actor.Props
 import akka.util.Timeout
-import scala.concurrent.duration._
 
+import scala.concurrent.duration._
 import scala.concurrent.{ExecutionContext, Future}
 import scala.util.{Failure, Success}
 
@@ -22,19 +23,45 @@ object UserService extends App {
     implicit val materializer: ActorMaterializer = ActorMaterializer()
 
     val service = "users"
+    val timeoutTime = 250 millisecond;
 
+    /*
+    ToDo: Find distributed and akka style implementation of UserRef lookup
+     */
+    var persistentUserActor = scala.collection.mutable.Map[Long, ActorRef]()
 
-    def createUser() : Future[Any] = {
-      implicit val timeout = Timeout(5 seconds)
-      val actorRef = system.actorOf(UserMsg.props());
-      actorRef ? CreateUser();
+    /*
+    Handles the given command for a UserActor by sending it with the ask pattern to the correct actor.
+    Returns actually a future of type UserCommunication.Event.
+    Giving userId -1 is no userId
+    */
+    def commandHandler(command: UserCommunication.Command, userId: Long): Future[Any] = {
+      implicit val timeout = Timeout(timeoutTime)
+      actorHandler(userId) ? command
+    }
+
+    /*
+    Hides the actor ref lookup from all the other functions, always use this as an endpoint to get actor refs by userId
+    ToDo: Find distributed and akka style implementation of UserRef lookup
+     */
+    def actorHandler(userId: Long) : ActorRef = {
+      (userId, persistentUserActor.get(userId)) match {
+        case (-1, _) =>
+          return system.actorOf(UserCommunication.props())
+        case (_, Some(actorRef)) =>
+          return actorRef
+        case (_, None) =>
+          return system.actorOf(UserCommunication.props())
+      }
     }
 
     val postCreateUserRoute: Route = {
       pathPrefix(service /  "create" ) {
         post{
           pathEndOrSingleSlash {
-            onSuccess(createUser()) { userId => complete("User created:")
+            onSuccess(commandHandler(CreateUser(), -1)) {
+              case UserCommunication.UserCreated(userId) => complete("User: " + userId + " was created.")
+              case _ => throw new Exception("A UserCreated event was expected, but a ")
             }
           }
         }
@@ -42,50 +69,67 @@ object UserService extends App {
     }
 
     val deleteRemoveUserRoute: Route = {
-      pathPrefix(service /  "remove" / LongNumber) { id ⇒
+      pathPrefix(service /  "remove" / LongNumber) { userId ⇒
         delete{
           pathEndOrSingleSlash {
-            complete("Remove User " + id)
+            pathEndOrSingleSlash {
+              onSuccess(commandHandler(DeleteUser(userId), userId)) {
+                case UserCommunication.UserDeleted(userId, status) => complete("User: " + userId + " was deleted: " + status)
+                case _ => throw new Exception("A UserDeleted event was expected, but a ")
+              }
+            }
           }
         }
       }
     }
 
     val getFindUserRoute: Route = {
-      pathPrefix(service /  "find" / LongNumber) { id ⇒
+      pathPrefix(service /  "find" / LongNumber) { userId ⇒
         get{
           pathEndOrSingleSlash {
-            complete("Find User " + id)
+            onSuccess(commandHandler(FindUser(userId), userId)) {
+              case UserCommunication.UserFound(userId, data) => complete("User: " + userId + " has: " + data.toString)
+              case _ => throw new Exception("A UserFound event was expected, but a ")
+            }
           }
         }
       }
     }
 
     val getCreditRoute: Route = {
-      pathPrefix(service /  "credit" / LongNumber) { id ⇒
+      pathPrefix(service /  "credit" / LongNumber) { userId ⇒
         get {
           pathEndOrSingleSlash {
-            complete("Get Credit " + id)
+            onSuccess(commandHandler(GetCredit(userId), userId)) {
+              case UserCommunication.CreditGot(userId, credit) => complete("User: " + userId + " has: " + credit)
+              case _ => throw new Exception("A CreditGot event was expected, but a ")
+            }
           }
         }
       }
     }
 
     val postSubtractCreditRoute: Route = {
-      pathPrefix(service /  "credit" / "subtract" / LongNumber / LongNumber) { (id, amount) ⇒
+      pathPrefix(service /  "credit" / "subtract" / LongNumber / LongNumber) { (userId, amount) ⇒
         post {
           pathEndOrSingleSlash {
-            complete("Subtract Credit " + amount + " from " + id)
+            onSuccess(commandHandler(SubtractCredit(userId, amount), userId)) {
+              case UserCommunication.CreditSubtracted(userId, amount, credit) => complete("User: " + userId + " credit was subtracted by " + amount + " to "+ credit)
+              case _ => throw new Exception("A CreditSubtracted event was expected, but a ")
+            }
           }
         }
       }
     }
 
     val postAddCreditRoute: Route = {
-      pathPrefix(service /  "credit" / "add" / LongNumber / LongNumber) { (id, amount) ⇒
+      pathPrefix(service /  "credit" / "add" / LongNumber / LongNumber) { (userId, amount) ⇒
         post {
           pathEndOrSingleSlash {
-            complete("Add Credit " + amount + " to " + id)
+            onSuccess(commandHandler(AddCredit(userId, amount), userId)) {
+              case UserCommunication.CreditAdded(userId, amount, credit) => complete("User: " + userId + " credit was increased by " + amount + " to "+ credit)
+              case _ => throw new Exception("A CreditAdded event was expected, but a ")
+            }
           }
         }
       }
