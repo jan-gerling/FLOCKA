cbc1e6b232f3b85a9463f779266e4121db7f3e69
Jan Gerling <jan.gerling@posteo.de>
2019-05-16 14:51:14 +0200

    Interfere pipeline with supervisor to validate useractor response

--- src/main/scala/org/flocka/Services/User/UserActorSupervisor.scala
+++ src/main/scala/org/flocka/Services/User/UserActorSupervisor.scala
@@ -1,6 +1,7 @@
 package org.flocka.Services.User
 
 import java.util.UUID.randomUUID
+
 import akka.pattern.ask
 import akka.pattern.pipe
 import UserCommunication._
@@ -8,8 +9,7 @@ import akka.util.Timeout
 import scala.concurrent.duration._
 import scala.collection.mutable
 import scala.concurrent.{ExecutionContext, Future}
-import akka.actor.{ActorRef, Props}
-import akka.http.scaladsl.server.Directives.{complete, onSuccess}
+import akka.actor.{Actor, ActorRef, Props, Status}
 import akka.persistence.{PersistentActor, SnapshotOffer}
 
 
@@ -50,9 +50,15 @@ class UserActorSupervisor() extends PersistentActor {
   /*
   Use pipe pattern to forward the actual command to the correct actor and then relay it to the asking actor.
   */
-  def commandHandler(command: UserCommunication.Command, userId: Long, sender: ActorRef): Future[Any] = {
-    (actorHandler(userId) ? command) pipeTo sender
+  def commandHandler(command: UserCommunication.Command, userId: Long, recipientTo: ActorRef, condition: Any => Boolean): Future[Any] = {
+    val actorFuture = (actorHandler(userId) ? command)
+    actorFuture.filter(condition).recover {
+      // When filter fails, it will have a java.util.NoSuchElementException
+      case m: NoSuchElementException => 0
+    }
+    actorFuture pipeTo recipientTo
   }
+
 
   /*
   Hides the actor ref lookup from all the other functions, always use this as an endpoint to get actor refs by userId
@@ -88,23 +94,60 @@ class UserActorSupervisor() extends PersistentActor {
   val snapShotInterval = 1000
   val receiveCommand: Receive = {
     case CreateUser() =>
-
-      val userId: Long = generateUserId()
-      val actorRef = createUser(userId.toString)
-      persist(UserActorCreated(userId)) { event =>
-        updateState(event)
-        commandHandler(CreateUser(), userId, sender())
-      }
+      val userId = generateUserId()
+      commandHandler(
+        CreateUser(),
+        userId,
+        sender(),
+        _ match {
+          case UserCommunication.UserCreated(resultId) => userId == resultId
+          case _ => false
+        })
     case DeleteUser(userId) =>
-      commandHandler(DeleteUser(userId), userId, sender())
+      commandHandler(
+        DeleteUser(userId),
+        userId,
+        sender(),
+        _ match {
+          case UserCommunication.UserDeleted(resultId, status) => userId == resultId && status
+          case _ => false
+        })
     case FindUser(userId) =>
-      commandHandler(FindUser(userId), userId, sender())
+      commandHandler(
+        FindUser(userId),
+        userId,
+        sender(),
+        _ match {
+          case UserCommunication.UserFound(resultId, _) => userId == resultId
+          case _ => false
+        })
     case GetCredit(userId) =>
-      commandHandler(GetCredit(userId), userId, sender())
+      commandHandler(
+        GetCredit(userId),
+        userId,
+        sender(),
+        _ match {
+          case UserCommunication.UserDeleted(resultId, _) => userId == resultId
+          case _ => false
+        })
     case AddCredit(userId, amount) =>
-      commandHandler(AddCredit(userId, amount), userId, sender())
+      commandHandler(
+        GetCredit(userId),
+        userId,
+        sender(),
+        _ match {
+          case UserCommunication.CreditAdded(resultId, resultAmount, _) => userId == resultId && resultAmount == amount
+          case _ => false
+        })
     case SubtractCredit(userId, amount) =>
-      commandHandler(SubtractCredit(userId, amount), userId, sender())
+      commandHandler(
+        GetCredit(userId),
+        userId,
+        sender(),
+        _ match {
+          case UserCommunication.CreditSubtracted(resultId, resultAmount, _) => userId == resultId && resultAmount == amount
+          case _ => false
+        })
 
     case command @ _  => throw new IllegalArgumentException(command.toString)
   }
