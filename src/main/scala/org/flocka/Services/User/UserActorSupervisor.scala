a386a49721ffe421a640c8a3e922a0993c9e4ee0
Jan Gerling <jan.gerling@posteo.de>
2019-05-16 15:59:48 +0200

    Refactoring and documentation for UserActorSupervisor

--- src/main/scala/org/flocka/Services/User/UserActorSupervisor.scala
+++ src/main/scala/org/flocka/Services/User/UserActorSupervisor.scala
@@ -1,7 +1,6 @@
 package org.flocka.Services.User
 
 import java.util.UUID.randomUUID
-
 import akka.pattern.ask
 import akka.pattern.pipe
 import UserCommunication._
@@ -9,20 +8,35 @@ import akka.util.Timeout
 import scala.concurrent.duration._
 import scala.collection.mutable
 import scala.concurrent.{ExecutionContext, Future}
-import akka.actor.{Actor, ActorRef, Props, Status}
+import akka.actor.{ActorRef, Props}
 import akka.persistence.{PersistentActor, SnapshotOffer}
 
-
+/*
+!Currently this is not used!
+This state saves all existing user actors related to this supervisor.
+Use this state to validate the existence of user actors.
+ToDO: figure out if this is necessary or could be done with some build in functionality
+ */
 case class SupervisorState(persistentUserActors: mutable.ListBuffer[Long]) {
   def updated(event: Event): SupervisorState = event match {
     case UserActorCreated(userID) =>
       copy(persistentUserActors += userID)
+    case UserActorDeleted(userID) =>
+      copy(persistentUserActors -= userID)
   }
 
   def size: Int = persistentUserActors.size
 }
 
+/*
+This Object stores the props to create a UserActorSupervisor.
+ */
 object UserActorSupervisor{
+  /*
+  Props used to create a new UserActorSupervisor.
+  "Props is a configuration class to specify options for the creation of actors."
+  For more details on props look here: https://doc.akka.io/docs/akka/2.5.5/scala/actors.html
+   */
   def props(): Props = Props(new UserActorSupervisor())
 }
 
@@ -42,106 +56,158 @@ class UserActorSupervisor() extends PersistentActor {
 
   implicit val ec: ExecutionContext = context.dispatcher
 
+  /*
+  Defines the service context for this actor, e.g. ../users/user-supervisor.
+   */
   val service = "user-supervisor"
-  val timeoutTime = 1000 millisecond;
+
+  /*
+  ToDo: find a good timeout time
+   */
+  val timeoutTime = 500 millisecond;
   implicit val timeout = Timeout(timeoutTime)
+
+  /*
+  Stores all the actors currently known to the supervisor to increase actorRef lookups.
+   */
   var knownUserActor = mutable.Map.empty[Long, ActorRef]
 
   /*
-  Use pipe pattern to forward the actual command to the correct actor and then relay it to the asking actor.
+  Use pipe pattern to forward the actual command to the correct actor and then relay it to the asking actor enforcing conditions.
+  command: The command to send to the actor
+  userId: userId of the addressed user, implicates the user actor
+  recipientTo: The asking actor, often the user service, to this actor the response from the user actor is send to+
+  condition: all conditions the response has to fulfill in order to be passed
+  createNew: Do you expect to create a new UserActor, only to be set true from CreateUser Command
   */
-  def commandHandler(command: UserCommunication.Command, userId: Long, recipientTo: ActorRef, condition: Any => Boolean): Future[Any] = {
-    val actorFuture = (actorHandler(userId) ? command)
-    actorFuture.filter(condition).recover {
-      // When filter fails, it will have a java.util.NoSuchElementException
-      case m: NoSuchElementException => 0
+  def commandHandler(command: UserCommunication.Command, userId: Long, recipientTo: ActorRef, condition: Any => Boolean, createNew: Boolean = false): Future[Any] = {
+    actorHandler(userId, createNew) match {
+      case Some(actorRef) =>
+        val actorFuture = actorRef ? command
+        //ToDo: how to handle unexpected/ unwanted results?
+        actorFuture.filter(condition).recover {
+          // When filter fails, it will have a java.util.NoSuchElementException
+          case m: NoSuchElementException => 0
+        }
+        actorFuture pipeTo recipientTo
+      case None =>
+        if (createNew == false) {throw new IllegalArgumentException("Can't find an existing user for user id: " + userId)}
+        else {throw new IllegalArgumentException("Can't create a new user for user id: " + userId)}
     }
-    actorFuture pipeTo recipientTo
   }
 
+  /*
+  Hides the actor ref lookup from all the other functions, always use this as an endpoint to get actor refs by userId.
+  Create new specifies whether this user is supposed to be created newly.
+  ToDo: Find distributed implementation of actorRef lookups maybe delegating this already to temporary actors?
+  */
+  def actorHandler(userId: Long, createNew: Boolean): Option[ActorRef] = {
+    (createNew, findActor(userId)) match{
+      case (false, Some(actorRef: ActorRef)) => return Some(actorRef)
+      case (true, Some(_)) => throw  new IllegalAccessException("You want to create a new user with id that already exists: " + userId)
+      case (false, None) => return None
+      case (true, None) => Some(createUser(userId.toString))
+    }
+  }
 
   /*
-  Hides the actor ref lookup from all the other functions, always use this as an endpoint to get actor refs by userId
-  ToDo: Find distributed and akka style implementation of UserRef lookup
+  Get the reference to an existing actor.
    */
-  def actorHandler(userId: Long): ActorRef = {
+  def findActor(userId: Long): Option[ActorRef] = {
     knownUserActor.get(userId) match {
       case Some(actorRef) =>
-        return actorRef
+        return Some(actorRef)
       case None =>
-        return getChild(userId.toString);
+        if(state.persistentUserActors.contains(userId)){
+          return Some(recoverUserActor(userId.toString))
+        }
+        else
+          return Some(createUser(userId.toString))
     }
   }
 
   /*
-  Get the child actor of this supervisor with the child id
-  */
-  def getChild(userId: String): ActorRef = {
-    return context.child(userId).getOrElse {
-      return createUser(userId)
-    }
-  }
-
+  Create a new actor with the given userId.
+  NEVER CALL THIS except you really know what you are doing and have a good reason. Use actorHandler instead.
+   */
   def createUser(userId: String): ActorRef ={
     return context.actorOf(UserActor.props(), userId)
   }
 
+  /*
+ Recover a new actor with the given userId.
+ NEVER CALL THIS except you really know what you are doing and have a good reason. Use actorHandler instead.
+  */
+  def recoverUserActor(userId: String): ActorRef ={
+    return context.actorOf(UserActor.props(), userId)
+  }
+
+  /*
+  Generate a userId for a user, this userId is also used to address the correct actor via the actor path.
+  DO NOT ALTER A userID after it was assigned.
+   */
   def generateUserId(): Long = {
     return Math.abs(randomUUID().getLeastSignificantBits)
   }
 
-  //TODO figure out good interval value
+  //TODO figure out good interval value for snapshots
   val snapShotInterval = 1000
+
+  /*
+  Receive the commands here and process them, to keep the communication running.
+   */
   val receiveCommand: Receive = {
-    case CreateUser() =>
+    case command @ CreateUser() =>
       val userId = generateUserId()
       commandHandler(
-        CreateUser(),
+        command,
         userId,
         sender(),
         _ match {
           case UserCommunication.UserCreated(resultId) => userId == resultId
           case _ => false
-        })
-    case DeleteUser(userId) =>
+        },
+        true
+      )
+    case command @ DeleteUser(userId) =>
       commandHandler(
-        DeleteUser(userId),
+        command,
         userId,
         sender(),
         _ match {
           case UserCommunication.UserDeleted(resultId, status) => userId == resultId && status
           case _ => false
         })
-    case FindUser(userId) =>
+    case command @ FindUser(userId) =>
       commandHandler(
-        FindUser(userId),
+        command,
         userId,
         sender(),
         _ match {
           case UserCommunication.UserFound(resultId, _) => userId == resultId
           case _ => false
         })
-    case GetCredit(userId) =>
+    case command @ GetCredit(userId) =>
       commandHandler(
-        GetCredit(userId),
+        command,
         userId,
         sender(),
         _ match {
           case UserCommunication.UserDeleted(resultId, _) => userId == resultId
           case _ => false
         })
-    case AddCredit(userId, amount) =>
+    case command @ AddCredit(userId, amount) =>
       commandHandler(
-        GetCredit(userId),
+        command,
         userId,
         sender(),
         _ match {
           case UserCommunication.CreditAdded(resultId, resultAmount, _) => userId == resultId && resultAmount == amount
           case _ => false
         })
-    case SubtractCredit(userId, amount) =>
+    case command @ SubtractCredit(userId, amount) =>
       commandHandler(
-        GetCredit(userId),
+        command,
         userId,
         sender(),
         _ match {
