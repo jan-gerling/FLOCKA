ce00711ff0325311a5b8d3efa4a1f7d7ab2e8a1e
pedro <pmfsilvestre@gmail.com>
2019-06-04 14:22:07 +0200

    Changed docker compose name in build_and_run.sh

--- build_and_run.sh
+++ build_and_run.sh
@@ -2,4 +2,4 @@
 
 sbt assembly
 docker build -t service-runner .
-export SEED0_IP=localhost; export MY_IP=localhost; docker-compose -f docker-compose2.yml up
+export SEED0_IP=localhost; export MY_IP=localhost; docker-compose -f docker-compose-local.yml up
