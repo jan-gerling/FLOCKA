81dd152324ead51c9a0973a23db20fae28e9e5f2
pedro <pmfsilvestre@gmail.com>
2019-06-07 11:51:28 +0200

    Separate runners and images for mongo and dynamo

--- run_user_service.sh
+++ run_user_service.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-docker run wdm2019akka/service-runner u -Dakka.cluster.seed-nodes.0=akka.tcp://flocka-user@$SEED0_IP:2551  -Dakka.remote.netty.tcp.hostname=$(curl http://169.254.169.254/latest/meta-data/local-ipv4) -Dakka.remote.netty.tcp.port=2551 -Dloadbalancer.user.uri=$USER_LB_URI -Dloadbalancer.stock.uri=$STOCK_LB_URI -Dloadbalancer.order.uri=$ORDER_LB_URI -Dloadbalancer.payment.uri=$PAYMENT_LB_URI -Dakka.contrib.persistence.mongodb.mongo.mongouri=$MONGO_URI
+docker run --network="host" wdm2019akka/service-runner u -Dakka.cluster.seed-nodes.0=akka.tcp://flocka-user@$SEED0_IP:2551  -Dakka.remote.netty.tcp.hostname=$(curl http://169.254.169.254/latest/meta-data/local-ipv4) -Dakka.remote.netty.tcp.port=2551 -Dloadbalancer.user.uri=$USER_LB_URI -Dloadbalancer.stock.uri=$STOCK_LB_URI -Dloadbalancer.order.uri=$ORDER_LB_URI -Dloadbalancer.payment.uri=$PAYMENT_LB_URI -Dakka.contrib.persistence.mongodb.mongo.mongouri=$MONGO_URI
