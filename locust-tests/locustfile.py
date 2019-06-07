5d1a34575669c7106386f6dfce947aebffde4934
albertoviguria <alberto.viguria@gmail.com>
2019-06-07 18:02:05 +0200

    locust test working with aws deployment

--- locust-tests/locustfile.py
+++ locust-tests/locustfile.py
@@ -5,68 +5,85 @@ USER_PORT = '8080'
 STOCK_PORT = '8081'
 ORDER_PORT = '8082'
 PAYMENT_PORT = '8083'
-def client_request(locust_user):
+
+USER_LB_URI="http://UserLB-78627843.eu-central-1.elb.amazonaws.com:8080"
+STOCK_LB_URI="http://StockLB-1528496191.eu-central-1.elb.amazonaws.com:8081"
+ORDER_LB_URI="http://OrderLB-2122101751.eu-central-1.elb.amazonaws.com:8082"
+PAYMENT_LB_URI="http://PaymentLB-1003200854.eu-central-1.elb.amazonaws.com:8083"
 
 
 # Users Service
 def create_user(locust_user):
-    response = locust_user.client.post(":%s/users/create" %(USER_PORT))
+    locust_user.client.base_url = USER_LB_URI
+    #response = locust_user.client.post("%s/users/create" %(USER_LB_URI))
+    response = locust_user.client.post("/users/create", catch_response=True)
     try:
         locust_user.user_id = re.search('\((.+?)\)', response.text).group(1)
+        response.success()
     except AttributeError:
-        locust_user.interrupt() 
-    return 
+        response.failure(response.text)
+        create_user(locust_user)
 
 def remove_user(locust_user):
-    response = locust_user.client.delete(":%s/users/remove/%s" %(USER_PORT, locust_user.user_id))
+    locust_user.client.base_url = USER_LB_URI
+    response = locust_user.client.delete("/users/remove/%s" %(locust_user.user_id))
     return response.text
 
 def find_user(locust_user):
-    response = locust_user.client.get(":%s/users/find/%s" %(USER_PORT, locust_user.user_id))
+    locust_user.client.base_url = USER_LB_URI
+    response = locust_user.client.get("/users/find/%s" %(locust_user.user_id))
     return response.text
 
 def get_credit(locust_user):
-    response = locust_user.client.get(":%s/users/credit/%s" %(USER_PORT, locust_user.user_id))
+    locust_user.client.base_url = USER_LB_URI
+    response = locust_user.client.get("/users/credit/%s" %(locust_user.user_id), catch_response=True, name="/users/credit/")
     try:
         credit = re.search(',(.+?)\)', response.text).group(1)
+        response.success()
         return credit
     except AttributeError:
+        response.failure(response.text)
         return "unknown"
 
 def subtract_credit(locust_user, credit):
-    response = locust_user.client.post(":%s/users/credit/subtract/%s/%i" %(USER_PORT, locust_user.user_id, credit))
+    locust_user.client.base_url = USER_LB_URI
+    response = locust_user.client.post("/users/credit/subtract/%s/%i" %(locust_user.user_id, credit), name="/users/credit/subtract/")
     if response.text[:16] == 'CreditSubtracted':
         operation_performed = re.search(',true,', response.text)
         if operation_performed is not None:
             locust_user.credit -= credit
+        else:
+            print(response.text)
     return response.text
 
 def add_credit(locust_user, credit):
-    response = locust_user.client.post(":%s/users/credit/add/%s/%i" %(USER_PORT, locust_user.user_id, credit), catch_response=True)
+    locust_user.client.base_url = USER_LB_URI
+    response = locust_user.client.post("/users/credit/add/%s/%i" %(locust_user.user_id, credit), name="/users/credit/add/")
     if response.text[:11] == 'CreditAdded':
         operation_performed = re.search(',true,', response.text)
         if operation_performed is not None:
             locust_user.credit += credit
-    else:
-        response.failure('')
-        print("error adding credit: "+response.text)
+        else:
+            print(response.text)
     return response.text
 
 # Order Service
 def create_order(locust_user):
-    response = locust_user.client.post(":%s/orders/create/%s" %(ORDER_PORT, locust_user.user_id), catch_response=True)
+    locust_user.client.base_url = ORDER_LB_URI
+    response = locust_user.client.post("/orders/create/%s" %(locust_user.user_id), catch_response=True, name="/orders/create/")
     try:
         order_id = re.search('\((.+?),', response.text).group(1)
         if response.text[:12] == 'OrderCreated':
             locust_user.my_orders.append(order_id)
+            response.success()
             return order_id
     except AttributeError:
-        response.failure('')
-        print("error creating order: "+response.text)
+        response.failure(response.text)
     
 
 def remove_order(locust_user, order_id):
-    response = locust_user.client.delete(":%s/orders/remove/%s" %(ORDER_PORT, order_id))
+    locust_user.client.base_url = ORDER_LB_URI
+    response = locust_user.client.delete("/orders/remove/%s" %(order_id))
     if response.text[:12] == 'OrderDeleted':
         try:
             locust_user.my_orders.remove(order_id)
@@ -75,19 +92,23 @@ def remove_order(locust_user, order_id):
     return response.text
 
 def find_order(locust_user, order_id):
-    response = locust_user.client.get(":%s/orders/find/%s" %(ORDER_PORT, order_id))
+    locust_user.client.base_url = ORDER_LB_URI
+    response = locust_user.client.get("/orders/find/%s" %(order_id))
     return response.text
 
 def order_add_item(locust_user, order_id, item_id):
-    #response = locust_user.client.post(":%s/orders/addItem/%s/%s" %(ORDER_PORT, order_id, item_id))
-    response = locust_user.client.post(":%s/orders/item/add/%s/%s" %(ORDER_PORT, order_id, item_id))
+    locust_user.client.base_url = ORDER_LB_URI
+    #response = locust_user.client.post("%s/orders/addItem/%s/%s" %(ORDER_LB_URI, order_id, item_id))
+    response = locust_user.client.post("/orders/item/add/%s/%s" %(order_id, item_id) , name="/orders/item/add/")
 
 def order_remove_item(locust_user, order_id, item_id):
-    #response = locust_user.client.post(":%s/orders/removeItem/%s/%s" %(ORDER_PORT, order_id, item_id))
-    response = locust_user.client.post(":%s/orders/item/remove/%s/%s" %(ORDER_PORT, order_id, item_id))
+    locust_user.client.base_url = ORDER_LB_URI
+    #response = locust_user.client.post("%s/orders/removeItem/%s/%s" %(ORDER_LB_URI, order_id, item_id))
+    response = locust_user.client.post("/orders/item/remove/%s/%s" %(order_id, item_id))
 
 def checkout_order(locust_user, order_id):
-    response = locust_user.client.post(":%s/orders/checkout/%s" %(ORDER_PORT, order_id))
+    locust_user.client.base_url = ORDER_LB_URI
+    response = locust_user.client.post("/orders/checkout/%s" %(order_id))
     if response.status_code == 200:
         try:
             locust_user.my_orders.remove(order_id)
@@ -98,20 +119,26 @@ def checkout_order(locust_user, order_id):
 
 # Stock Service
 def stock_availability(locust_user, item_id):
-    return locust_user.client.get(":%s/stock/availability/%s" %(STOCK_PORT, item_id))
+    locust_user.client.base_url = STOCK_LB_URI
+    return locust_user.client.get("/stock/availability/%s" %(item_id))
     
 def add_stock(locust_user, item_id, quantity):
-    return locust_user.client.post(":%s/stock/add/%s/%s" %(STOCK_PORT, item_id, quantity))
+    locust_user.client.base_url = STOCK_LB_URI
+    return locust_user.client.post("/stock/add/%s/%s" %(item_id, quantity), name="/stock/add/")
 
 def subtract_stock(locust_user, item_id, quantity):
-    return locust_user.client.post(":%s/stock/subtract/%s/%s" %(STOCK_PORT, item_id, quantity))
+    locust_user.client.base_url = STOCK_LB_URI
+    return locust_user.client.post("/stock/subtract/%s/%s" %(item_id, quantity))
 
 def create_item(locust_user):
-    response = locust_user.client.post(":%s/stock/item/create/" %(STOCK_PORT), catch_response=True)
+    locust_user.client.base_url = STOCK_LB_URI
+    response = locust_user.client.post("/stock/item/create", catch_response=True)
     try:
-        return re.search('\((.+?)\)', response.text).group(1)
+        item_id = re.search('\((.+?)\)', response.text).group(1)
+        response.success()
+        return item_id
     except AttributeError:
-        response.failure('No item created')
+        response.failure(response.text)
         return None
 
 # aux functions
