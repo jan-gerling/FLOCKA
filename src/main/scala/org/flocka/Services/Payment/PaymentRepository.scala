49599f823a0e2572689549f6c8b936eacc531f12
Jan Gerling <jan.gerling@posteo.de>
2019-06-08 12:19:48 +0200

    refactored payment pay to handle connection errors

--- src/main/scala/org/flocka/Services/Payment/PaymentRepository.scala
+++ src/main/scala/org/flocka/Services/Payment/PaymentRepository.scala
@@ -1,22 +1,20 @@
 package org.flocka.Services.Payment
 
-import java.util.concurrent.TimeoutException
-
-import akka.actor.{Props, _}
-import akka.http.scaladsl.Http
+import akka.actor.Props
 import akka.http.scaladsl.model.{HttpMethods, HttpRequest, HttpResponse}
-import akka.http.scaladsl.settings.{ClientConnectionSettings, ConnectionPoolSettings}
 import akka.persistence.SnapshotOffer
 import com.typesafe.config.{Config, ConfigFactory}
 import org.flocka.ServiceBasics.MessageTypes.Event
-import org.flocka.ServiceBasics.PersistentActorBase.InvalidPaymentException
+import org.flocka.ServiceBasics.PersistentActorBase.{InvalidOperationException, InvalidPaymentException}
 import org.flocka.ServiceBasics.{MessageTypes, PersistentActorBase, PersistentActorState}
 import org.flocka.Services.Payment.PaymentServiceComs._
-import org.flocka.Utils.PushOutHashmapQueueBuffer
-
+import org.flocka.Utils.{PushOutHashmapQueueBuffer}
+import akka.actor.ActorSystem
+import akka.http.scaladsl.Http
+import akka.stream.StreamTcpException
 import scala.collection.mutable
 import scala.concurrent.duration._
-import scala.concurrent.{Await, ExecutionContext, Future}
+import scala.concurrent.{ExecutionContext}
 
 /**
   * PaymentActor props and custom exceptions for a PaymentActor.
@@ -50,10 +48,10 @@ case class PaymentRepositoryState(payments: mutable.Map[Long, PaymentState], cur
   override var doneOperations = currentOperations
 
   def updated(event: MessageTypes.Event): PaymentRepositoryState = event match {
-    case PaymentPayed(userId, orderId, true, operationId) =>
+    case PaymentPayed(userId, orderId, status, operationId) =>
       doneOperations.push(operationId, event)
       copy(payments += orderId -> new PaymentState(userId, orderId, true).updated(event))
-    case PaymentCanceled(userId, orderId, false, operationId) =>
+    case PaymentCanceled(userId, orderId, status, operationId) =>
       doneOperations.push(operationId, event)
       copy(payments += orderId -> new PaymentState(userId, orderId, false).updated(event))
     case _ => throw new IllegalArgumentException(event.toString + "is not a valid event for PaymentActor.")
@@ -75,13 +73,14 @@ class PaymentRepository extends PersistentActorBase {
   implicit val executor: ExecutionContext = system.dispatcher
 
   val MAX_NUM_TRIES = 3
-  val TIMOUT_TIME = 500 millisecond
+  val TIMEOUT_TIME  = 10000
 
   val config: Config = ConfigFactory.load("payment-service.conf")
   val passivateTimeout: FiniteDuration = config.getInt("sharding.passivate-timeout") seconds
   val snapShotInterval: Int = config.getInt("sharding.snapshot-interval")
 
-  val loadBalancerURI: String = config.getString("loadbalancer.payment.uri")
+  val loadBalancerURIOrder: String = config.getString("loadbalancer.order.uri")
+  val loadBalancerURIUser: String = config.getString("loadbalancer.user.uri")
 
   // Since we have millions of users, we should passivate quickly
   context.setReceiveTimeout(passivateTimeout)
@@ -107,34 +106,66 @@ class PaymentRepository extends PersistentActorBase {
 
   def buildResponseEvent(request: MessageTypes.Request): Event = {
     request match {
-      case PayPayment(userId, orderId, operationId) =>
-        val orderUri = loadBalancerURI + "/orders/find/" + orderId
-        val futureOrder: Future[HttpResponse] = sendRequest(orderUri)
+      case pay@ PayPayment(userId, orderId, operationId) =>
+        var resultEvent: PaymentPayed = new PaymentPayed(-1, -1, false, -1)
+        val orderUri = loadBalancerURIOrder + "/orders/find/" + orderId
+        val future = for {
+          orderDetails: HttpResponse       <- Http(system).singleRequest(HttpRequest(method = HttpMethods.GET, uri = orderUri))
+          creditDetails: HttpResponse      <-
+          orderDetails.entity.toString.contains("OrderFound") match {
+            case true => val amount = getTotalOrderCost(orderDetails.entity.toString)
+              val decreaseCreditUri = loadBalancerURIUser + "/users/credit/subtract/" + pay.userId + "/" + amount
+              Http(system).singleRequest(HttpRequest(method = HttpMethods.POST, uri = decreaseCreditUri))
+            case false => throw new InvalidOperationException(persistenceId, pay.toString)
+          }} yield {
+          val creditSubtracted: Boolean  = creditDetails.entity.toString.contains("CreditSubtracted") && creditDetails.entity.toString.contains("true")
+          resultEvent = PaymentPayed(userId, orderId, creditSubtracted, operationId)
+        }
+        future.recover {
+          case _: InvalidOperationException => resultEvent = PaymentPayed(userId, orderId, false, operationId)
+          case _: StreamTcpException => resultEvent = PaymentPayed(userId, orderId, false, operationId)
+          case exception@ _ => println(exception); resultEvent = PaymentPayed(userId, orderId, false, operationId)
+        }
 
-        Await.result(futureOrder.map { response =>
-          val amount = getTotalOrderCost(response.toString())
-          val decreaseCreditUri = loadBalancerURI + "/users/credit/subtract/" + userId + "/" + amount
-          val futureCredit: Future[HttpResponse] = sendRequest(decreaseCreditUri)
-          Await.result[Event](futureCredit.map { response =>
-            val success: Boolean = response.toString.contains("CreditSubtracted") && response.toString.contains("true")
-            PaymentPayed(userId, orderId, success, operationId)
-          }, TIMOUT_TIME)
-        }, TIMOUT_TIME)
+        var elapsedTime: Long = 0
+        while(resultEvent.userId < 0){
+          if(elapsedTime > TIMEOUT_TIME){
+            throw new Exception("Timeout!")
+          }
+          elapsedTime += 15
+          Thread.sleep(15)
+        }
+        resultEvent
+      case cancelPay@ CancelPayment(userId, orderId, operationId) =>
+        var resultEvent: PaymentCanceled = PaymentCanceled(-1, -1, false, -1)
+        val orderUri = loadBalancerURIOrder + "/orders/find/" + orderId
+        val future = for {
+          orderDetails: HttpResponse       <- Http(system).singleRequest(HttpRequest(method = HttpMethods.GET, uri = orderUri))
+          creditDetails: HttpResponse      <-
+          orderDetails.entity.toString.contains("OrderFound") match {
+            case true => val amount = getTotalOrderCost(orderDetails.entity.toString)
+              val increaseCreditUri = loadBalancerURIUser + "/users/credit/add/" + userId + "/" + amount
+              Http(system).singleRequest(HttpRequest(method = HttpMethods.POST, uri = increaseCreditUri))
+            case false => throw new InvalidOperationException(persistenceId, cancelPay.toString)
+          }} yield {
+          val creditAdded: Boolean  = creditDetails.toString.contains("CreditAdded") && creditDetails.toString.contains("true")
+          resultEvent = PaymentCanceled(userId, orderId, creditAdded, operationId)
+        }
+        future.recover {
+          case _: InvalidOperationException => resultEvent = PaymentCanceled(userId, orderId, false, operationId)
+          case _: StreamTcpException => resultEvent = PaymentCanceled(userId, orderId, false, operationId)
+          case exception@ _ => println(exception); resultEvent = PaymentCanceled(userId, orderId, false, operationId)
+        }
 
-      case CancelPayment(userId, orderId, operationId) =>
-        val orderUri = loadBalancerURI + "/orders/find/" + orderId
-        val futureOrder: Future[HttpResponse] = sendRequest(orderUri)
-
-        Await.result(futureOrder.map { response =>
-          val amount = getTotalOrderCost(response.toString())
-          val decreaseCreditUri = loadBalancerURI + "/users/credit/add/" + userId + "/" + amount
-          val futureCredit: Future[HttpResponse] = sendRequest(decreaseCreditUri)
-          Await.result[Event](futureCredit.map { response =>
-            val success: Boolean = response.toString.contains("CreditAdded") && response.toString.contains("true")
-            PaymentCanceled(userId, orderId, success, operationId)
-          }, TIMOUT_TIME)
-        }, TIMOUT_TIME)
-
+        var elapsedTime: Long = 0
+        while(resultEvent.userId < 0){
+          if(elapsedTime > TIMEOUT_TIME){
+            throw new Exception("Timeout!")
+          }
+          elapsedTime += 15
+          Thread.sleep(15)
+        }
+        resultEvent
       case GetPaymentStatus(orderId) =>
         val paymentState: PaymentState = getPaymentState(orderId).getOrElse(throw new InvalidPaymentException(orderId.toString))
         return PaymentStatusFound(orderId, paymentState.status)
@@ -160,35 +191,17 @@ class PaymentRepository extends PersistentActorBase {
     }
   }
 
-
   private def getTotalOrderCost(response: String) : Long ={
     var amount : Long = 0
     var currTuple = ""
     if (response.contains("List((")) { //has more than one item
-      val listOfTuples = response.split("List(")(1).split("), (")
+      val listOfTuples = response.split("List\\(")(1).split("\\), \\(")
       for (tuple <- listOfTuples) {
-        currTuple = tuple replaceAll ("(","")
-        currTuple = tuple replaceAll (")","")
+        currTuple = tuple replaceAll ("\\(","")
+        currTuple = tuple replaceAll ("\\)","")
         amount += currTuple.split(",")(1).toLong
       }
     }
     return amount
-  }
-
-  private def sendRequest(path: String, numTries: Int = 0)
-                         (implicit executor: ExecutionContext, system: ActorSystem): Future[HttpResponse] = {
-
-
-    val connectionSettings = ClientConnectionSettings(system).withIdleTimeout(numTries seconds)
-    val connectionPoolSettings: ConnectionPoolSettings = ConnectionPoolSettings(system).withConnectionSettings(connectionSettings)
-
-    return Http()(system).singleRequest(HttpRequest(method = HttpMethods.POST, uri = path.toString), settings = connectionPoolSettings).recover{
-      case exception: TimeoutException =>
-        if(numTries >= MAX_NUM_TRIES){
-          return Future.failed(exception)
-        }else {
-          return sendRequest(path, numTries + 1)
-        }
-    }
   }
 }
